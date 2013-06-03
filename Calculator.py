6c7b21b06aab8583f2675063614a2063198802e7
Free1man <igalperin@ebosphorus.com>
2013-06-03 20:47:19 +1100

    calc update

--- Calculator.py
+++ Calculator.py
@@ -4,31 +4,44 @@ def additionBin():
     firstNumber = int(raw_input("Первое число: "))
     secondNumber = int(raw_input("Второе число: "))
 
-    result = int(firstNumber - secondNumber)
-    n = ""
+    result = int(firstNumber + secondNumber)
+    shownResult = ""
     if result > 0:
         while result > 0:
-            y = str(result % 2)
-            n = y + n
+            modulo = str(result % 2)
+            shownResult = modulo + shownResult
             result = int(result / 2)
-        return n
+
+        if 8 > len(shownResult) > 0:
+            shownResult = 'b'+'0'*(8 - len(shownResult))+str(shownResult)
+        elif 16 > len(shownResult) > 8:
+            shownResult = 'b'+'0'*(16 - len(shownResult))+str(shownResult)
+        elif 32 > len(shownResult) > 16:
+            shownResult = 'b'+'0'*(32 - len(shownResult))+str(shownResult)
+        elif 64 > len(shownResult) > 32:
+            shownResult = 'b'+'0'*(64 - len(shownResult))+str(shownResult)
+
 
     elif result < 0:
-        z = -result
-        while z > 0:
-            y = str(z % 2)
-            n = y + n
-            z = int(z / 2)
-    if 8 > len(n) > 0:
-        n2 = 'b1'+'0'*(8 - len(n)-1)+str(n)
-    elif 16 > len(n) > 8:
-        n2 = 'b1'+'0'*(16 - len(n)-1)+str(n)
-    elif 32 > len(n) > 16:
-        n2 = 'b1'+'0'*(32 - len(n)-1)+str(n)
-    elif 64 > len(n) > 32:
-        n2 = 'b1'+'0'*(64 - len(n)-1)+str(n)
+        result = -result
+        while result > 0:
+            modulo = str(result % 2)
+            shownResult = modulo + shownResult
+            result = int(result / 2)
+        if 8 > len(shownResult) > 0:
+            shownResult = 'b1'+'0'*(8 - len(shownResult)-1)+str(shownResult)
+        elif 16 > len(shownResult) > 8:
+            shownResult = 'b1'+'0'*(16 - len(shownResult)-1)+str(shownResult)
+        elif 32 > len(shownResult) > 16:
+            shownResult = 'b1'+'0'*(32 - len(shownResult)-1)+str(shownResult)
+        elif 64 > len(shownResult) > 32:
+            shownResult = 'b1'+'0'*(64 - len(shownResult)-1)+str(shownResult)
 
-    return n2
+    elif result == 0:
+
+        print 0
+
+    return shownResult
 
 
 
@@ -39,29 +52,29 @@ def additionHex():
     secondNumber = int(raw_input("Второе число: "))
 
     result = int(firstNumber + secondNumber)
-    n = ""
+    shownResult = ""
 
     while result > 0:
-        y = result % 16
-        if y == 10:
-            y = 'A'
-        if y == 11:
-            y = 'B'
-        if y == 12:
-            y = 'C'
-        if y == 13:
-            y = 'D'
-        if y == 14:
-            y = 'E'
-        if y == 15:
-            y = 'F'
-        if y < 10:
-            y = str(y)
+        modulo = result % 16
+        if modulo == 10:
+            modulo = 'A'
+        if modulo == 11:
+            modulo = 'B'
+        if modulo == 12:
+            modulo = 'C'
+        if modulo == 13:
+            modulo = 'D'
+        if modulo == 14:
+            modulo = 'E'
+        if modulo == 15:
+            modulo = 'F'
+        if modulo < 10:
+            modulo = str(modulo)
 
-        n = y + n
+        shownResult = modulo + shownResult
         result = int(result / 16)
 
-    return (n)
+    return (shownResult)
 
 
 def subtractionBin():
@@ -70,31 +83,39 @@ def subtractionBin():
     secondNumber = int(raw_input("Второе число: "))
 
     result = int(firstNumber - secondNumber)
-    n = ""
+    shownResult = ""
 
     if result > 0:
         while result > 0:
-            y = str(result % 2)
-            n = y + n
+            modulo = str(result % 2)
+            shownResult = modulo + shownResult
             result = int(result / 2)
-        return n
+        if 8 > len(shownResult) > 0:
+            shownResult = 'b'+'0'*(8 - len(shownResult))+str(shownResult)
+        elif 16 > len(shownResult) > 8:
+            shownResult = 'b'+'0'*(16 - len(shownResult))+str(shownResult)
+        elif 32 > len(shownResult) > 16:
+            shownResult = 'b'+'0'*(32 - len(shownResult))+str(shownResult)
+        elif 64 > len(shownResult) > 32:
+            shownResult = 'b'+'0'*(64 - len(shownResult))+str(shownResult)
+
 
     elif result < 0:
-        z = -result
-        while z > 0:
-            y = str(z % 2)
-            n = y + n
-            z = int(z / 2)
-    if 8 > len(n) > 0:
-        n2 = 'b1'+'0'*(8 - len(n)-1)+str(n)
-    elif 16 > len(n) > 8:
-        n2 = 'b1'+'0'*(16 - len(n)-1)+str(n)
-    elif 32 > len(n) > 16:
-        n2 = 'b1'+'0'*(32 - len(n)-1)+str(n)
-    elif 64 > len(n) > 32:
-        n2 = 'b1'+'0'*(64 - len(n)-1)+str(n)
+        result = -result
+        while result > 0:
+            modulo = str(result % 2)
+            shownResult = modulo + shownResult
+            result = int(result / 2)
+        if 8 > len(shownResult) > 0:
+            shownResult = 'b1'+'0'*(8 - len(shownResult)-1)+str(shownResult)
+        elif 16 > len(shownResult) > 8:
+            shownResult = 'b1'+'0'*(16 - len(shownResult)-1)+str(shownResult)
+        elif 32 > len(shownResult) > 16:
+            shownResult = 'b1'+'0'*(32 - len(shownResult)-1)+str(shownResult)
+        elif 64 > len(shownResult) > 32:
+            shownResult = 'b1'+'0'*(64 - len(shownResult)-1)+str(shownResult)
 
-    return n2
+    return shownResult
 
 def subtractionHex():
     print "Введите два чилса для вычитания"
@@ -102,237 +123,30 @@ def subtractionHex():
     secondNumber = int(raw_input("Второе число: "))
 
     result = int(firstNumber - secondNumber)
-    n = ""
+    shownResult = ""
 
     if result > 0:
-        result = int(firstNumber + secondNumber)
-        n = ""
 
         while result > 0:
-            y = result % 16
-            if y == 10:
-                y = 'A'
-            if y == 11:
-                y = 'B'
-            if y == 12:
-                y = 'C'
-            if y == 13:
-                y = 'D'
-            if y == 14:
-                y = 'E'
-            if y == 15:
-                y = 'F'
-            if y < 10:
-                y = str(y)
+            modulo = result % 16
+            if modulo == 10:
+                modulo = 'A'
+            if modulo == 11:
+                modulo = 'B'
+            if modulo == 12:
+                modulo = 'C'
+            if modulo == 13:
+                modulo = 'D'
+            if modulo == 14:
+                modulo = 'E'
+            if modulo == 15:
+                modulo = 'F'
+            if modulo < 10:
+                modulo = str(modulo)
 
-            n = y + n
+            shownResult = modulo + shownResult
             result = int(result / 16)
-        print (n)
-
-    elif result < 0:
-
-        print "---"
-
-
-
-print "1: +"
-print "2: -"
-print "0: выход"
-
-while True:
-
-    choice = int(raw_input("Выберите действие из списка  "))
-
-    if choice == 1:
-        print "1: BIN "
-        print "2: HEX "
-
-        while True:
-
-            choice = int(raw_input("BIN или HEX "))
-
-            if choice == 1:
-                print 'Cложение BIN'
-                print additionBin()
-
-            elif choice == 2:
-                print 'Cложение HEX'
-                print additionHex()
-            else:
-                print "введите 1 или 2 "
-
-    elif choice == 2:
-        print "1: BIN "
-        print "2: HEX "
-
-        while True:
-
-            choice = int(raw_input("BIN или HEX "))
-
-            if choice == 1:
-                print 'Вычитание BIN'
-                print subtractionBin()
-
-
-            elif choice == 2:
-                print 'Вычитание HEX'
-                print subtractionHex()
-            else:
-                print "введите 1 или 2 "
-
-    elif choice == 0:
-        exit()
-    else:
-        print "введите 1 или 2 "
-
-# -*- coding: utf-8 -*-
-def additionBin():
-    print "Введите два чилса для сложения"
-    firstNumber = int(raw_input("Первое число: "))
-    secondNumber = int(raw_input("Второе число: "))
-
-    result = int(firstNumber + secondNumber)
-    n = ""
-    if result > 0:
-        while result > 0:
-            y = str(result % 2)
-            n = y + n
-            result = int(result / 2)
-
-        if 8 > len(n) > 0:
-            n2 = 'b'+'0'*(8 - len(n))+str(n)
-        elif 16 > len(n) > 8:
-            n2 = 'b'+'0'*(16 - len(n))+str(n)
-        elif 32 > len(n) > 16:
-            n2 = 'b'+'0'*(32 - len(n))+str(n)
-        elif 64 > len(n) > 32:
-            n2 = 'b'+'0'*(64 - len(n))+str(n)
-
-    elif result < 0:
-        z = -result
-        while z > 0:
-            y = str(z % 2)
-            n = y + n
-            z = int(z / 2)
-
-        if 8 > len(n) > 0:
-            n2 = 'b1'+'0'*(8 - len(n)-1)+str(n)
-        elif 16 > len(n) > 8:
-            n2 = 'b1'+'0'*(16 - len(n)-1)+str(n)
-        elif 32 > len(n) > 16:
-            n2 = 'b1'+'0'*(32 - len(n)-1)+str(n)
-        elif 64 > len(n) > 32:
-            n2 = 'b1'+'0'*(64 - len(n)-1)+str(n)
-
-    return n2
-
-
-
-
-def additionHex():
-    print "Введите два чилса для сложения"
-    firstNumber = int(raw_input("Первое число: "))
-    secondNumber = int(raw_input("Второе число: "))
-
-    result = int(firstNumber + secondNumber)
-    n = ""
-
-    while result > 0:
-        y = result % 16
-        if y == 10:
-            y = 'A'
-        if y == 11:
-            y = 'B'
-        if y == 12:
-            y = 'C'
-        if y == 13:
-            y = 'D'
-        if y == 14:
-            y = 'E'
-        if y == 15:
-            y = 'F'
-        if y < 10:
-            y = str(y)
-
-        n = y + n
-        result = int(result / 16)
-
-    return (n)
-
-
-def subtractionBin():
-    print "Введите два чилса для вычитания"
-    firstNumber = int(raw_input("Первое число: "))
-    secondNumber = int(raw_input("Второе число: "))
-
-    result = int(firstNumber - secondNumber)
-    n = ""
-
-    if result > 0:
-        while result > 0:
-            y = str(result % 2)
-            n = y + n
-            result = int(result / 2)
-
-        if 8 > len(n) > 0:
-            n2 = 'b'+'0'*(8 - len(n))+str(n)
-        elif 16 > len(n) > 8:
-            n2 = 'b'+'0'*(16 - len(n))+str(n)
-        elif 32 > len(n) > 16:
-            n2 = 'b'+'0'*(32 - len(n))+str(n)
-        elif 64 > len(n) > 32:
-            n2 = 'b'+'0'*(64 - len(n))+str(n)
-
-    elif result < 0:
-        z = -result
-        while z > 0:
-            y = str(z % 2)
-            n = y + n
-            z = int(z / 2)
-
-        if 8 > len(n) > 0:
-            n2 = 'b1'+'0'*(8 - len(n)-1)+str(n)
-        elif 16 > len(n) > 8:
-            n2 = 'b1'+'0'*(16 - len(n)-1)+str(n)
-        elif 32 > len(n) > 16:
-            n2 = 'b1'+'0'*(32 - len(n)-1)+str(n)
-        elif 64 > len(n) > 32:
-            n2 = 'b1'+'0'*(64 - len(n)-1)+str(n)
-
-    return n2
-
-def subtractionHex():
-    print "Введите два чилса для вычитания"
-    firstNumber = int(raw_input("Первое число: "))
-    secondNumber = int(raw_input("Второе число: "))
-
-    result = int(firstNumber - secondNumber)
-    n = ""
-
-    if result > 0:
-        result = int(firstNumber + secondNumber)
-        n = ""
-
-        while result > 0:
-            y = result % 16
-            if y == 10:
-                y = 'A'
-            if y == 11:
-                y = 'B'
-            if y == 12:
-                y = 'C'
-            if y == 13:
-                y = 'D'
-            if y == 14:
-                y = 'E'
-            if y == 15:
-                y = 'F'
-            if y < 10:
-                y = str(y)
-
-            n = y + n
-            result = int(result / 16)
-        print (n)
+        print (shownResult)
 
     elif result < 0:
 
