37fe8cf325fa691836015360a382f37d9080e86f
Ilya Galperin <igalperin@ebosphorus.com>
2013-06-29 09:09:19 +1100

    ui update

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 from Tkinter import *
 
-import datetime
+import tkMessageBox
 
-import ctypes
+import datetime
 
 import ttk
 
@@ -15,9 +15,9 @@ def writeEventToFile(event):
     data.write(reminder.get()+"\n")
     data.close()
 
-    text = 'Your event ' + eventName.get() + ' '+' will start at ' + date.get() + ' you will be reminded %s day(s) before' % reminder.get()
-    MessageBox = ctypes.windll.user32.MessageBoxA
-    MessageBox(None, text, 'Reminder', 0)
+    text = 'Your event ' + eventName.get() + ' ' + ' will start at ' + date.get() + ' you will be reminded %s day(s) before' % reminder.get()
+
+    tkMessageBox.showinfo('Reminder', text)
 
 def readEventFromFile(event):
     data = open("data2.txt", "r")
@@ -30,26 +30,25 @@ def readEventFromFile(event):
 
         x = text[datefromfile]
         y = text[reminder]
-        checkEvent (x,int(y))
+        checkEvent(x, int(y))
         datefromfile = datefromfile + 3
         reminder = reminder + 3
     data.close()
 
     data = open("data1.txt", "r")
     text = data.read()
-    if len(text)<1:
-        MessageBox = ctypes.windll.user32.MessageBoxA
-        MessageBox(None, "No reminders", 'Reminder', 0)
-    elif len(text)>1:
-        MessageBox = ctypes.windll.user32.MessageBoxA
-        MessageBox(None, text, 'Reminder', 0)
+    if len(text) < 1:
+        tkMessageBox.showinfo('Reminder', "No reminders")
+    elif len(text) > 1:
+        tkMessageBox.showinfo('Reminder', text)
+
     open('data1.txt', 'w').close()
 
-def checkEvent(datefromfile , reminder):
-
+def checkEvent(datefromfile, reminder):
 
     currentDate = datetime.datetime.now()
-    eventDate = datetime.datetime.strptime(datefromfile,"%Y-%m-%d")
+    currentDate = currentDate.replace(hour=0, minute=0, second=0, microsecond=0)
+    eventDate = datetime.datetime.strptime(datefromfile, "%Y-%m-%d")
     reminderDate = eventDate - datetime.timedelta(days=reminder)
 
     if currentDate > eventDate:
@@ -59,7 +58,7 @@ def checkEvent(datefromfile , reminder):
         print reminderDate
         print reminder
         print 'missed'
-    elif currentDate <= reminderDate:
+    elif currentDate >= reminderDate:
         data = open("data1.txt", "a")
         data.write('You will have upcoming event on '+str(datefromfile)+'\n')
         data.close()
@@ -76,26 +75,26 @@ __name__ == '__main__'
 root = Tk()
 root.geometry("250x200")
 
-dateLabel = Label(root,text="Date YYYY-MM-DD")
-dateLabel.pack( side = TOP)
-date = Entry(bd =2)
+dateLabel = Label(root, text="Date YYYY-MM-DD")
+dateLabel.pack(side=TOP)
+date = Entry(bd=2)
 date.pack()
 
-eventNameLabel = Label(root,text="Event Name")
-eventNameLabel.pack( side = TOP)
-eventName = Entry(bd =2)
+eventNameLabel = Label(root, text="Event Name")
+eventNameLabel.pack(side=TOP)
+eventName = Entry(bd=2)
 eventName.pack()
 
-reminderLabel = Label(root,text="Remind days before")
-reminderLabel.pack( side = TOP)
-reminder = Entry(bd =2)
+reminderLabel = Label(root, text="Remind days before")
+reminderLabel.pack( side=TOP)
+reminder = Entry(bd=2)
 reminder.pack()
 
 root.wm_title("Reminder")
 Read = ttk.Button(root, text="Read", width=30)
-Read.bind("<Button-1>",readEventFromFile)
+Read.bind("<Button-1>", readEventFromFile)
 Write = ttk.Button(root, text="Write", width=30)
-Write.bind("<Button-1>",writeEventToFile)
+Write.bind("<Button-1>", writeEventToFile)
 Write.pack()
 Read.pack()
 
